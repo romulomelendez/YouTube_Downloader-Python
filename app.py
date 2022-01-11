04f38552e5a8e716e461f3a814f87c49f35433f1
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-01-11 19:37:10 -0300

    update in css style

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, request, render_template
+from markupsafe import escape
 from pytube import YouTube
 
 app = Flask(__name__)
+
+def showResolutions(url):
+
+  try:
+    yt = YouTube(url)
+    videos = yt.streams.filter(progressive=True)
+    for video in videos:
+      return render_template('home.html', op1=video.fps)
+      #print(f'Resolution: {video.resolution} - FPS: {video.fps}')
+  except:
+    print("An error occured")
+  finally:
+    print("The 'try except' is finished")
+
 
 @app.route('/')
 def index():
@@ -10,23 +25,10 @@ def index():
 
 @app.route('/', methods = ['POST'])
 def getLink():
-    text = request.form['video-link']
-    showResolutions(text)
-    return "All's good!"
-
-
-def showResolutions(url):
-
-  try:
-    yt = YouTube(url)
-    videos_res = yt.streams.filter(progressive=True)
-    for item in videos_res:
-        print('Resolution: {} | FPS: {}'.format(item.resolution, item.fps))
-  except:
-    print('An error occured')
-  finally:
-    print("The 'try except' is finished")
+    url = request.form['video-link']
+    showResolutions(url)
+    return ''
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
