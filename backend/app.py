8d1f46ec839fb837cd3e990663d574d00fdd92b9
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-02-14 22:26:40 -0300

    backend returning data to frontend

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,27 @@
+import json
 from flask import Flask, request
 from Downloader import Downloader
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+downloader = Downloader()
 
 
 @app.route('/', methods=['POST'])
 def get_url():
-    video_url = request.form['video_url']
-    Downloader(video_url)
+    print('cheguei no backend!')
+    video_url = request.json['videoUrl']
+    # downloader.get_video(video_url)
+    # data = downloader.build_video_data()
+    return json.dumps(video_url)
 
-    return 'OK'
+
+@app.route('/download/<itag>', methods=['GET'])
+def download_video(itag):
+    video_download_response = downloader.download(itag)
+    return f"{video_download_response}"
 
 
 if __name__ == '__main__':
--- frontend/src/pages/Downloader/index.tsx
+++ frontend/src/pages/Downloader/index.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react"
+
+import axios from "axios"
+
 export const Downloader: React.FC = () => {
 
+    const [videoUrl, setVideoUrl] = useState("")
+
+    const getVideoData = async () => {
+        
+        const videoResponse = await axios.post("http://localhost:5000", {
+            "videoUrl": videoUrl
+        })
+
+        console.log(videoResponse)
+    }
+
     return (
-        <form action="http://localhost:5000/" method="POST">
-            <input type="text" name="video_url" />
-            <button type="submit">Download</button>
+        <form>
+            <input type="text" onChange={e => setVideoUrl(e.target.value)} />
+            <button type="button" onClick={getVideoData}>Download</button>
         </form>
     )
 }
