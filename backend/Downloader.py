3be971947877ee1152515b46353f75c48d547ae2
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-02-20 14:57:39 -0300

    video streams  format response data updated

--- backend/Downloader.py
+++ backend/Downloader.py
@@ -12,7 +12,7 @@ class Downloader:
         yt = YouTube(url)
         self.__set_video_title(yt.title)
         self.__set_video_thumbnail(yt.thumbnail_url)
-        # self.__set_video_streams(yt.streams.filter(progressive=True))
+        self.__set_video_streams(yt.streams.filter(progressive=True))
 
 
     def __set_video_title(self, video_title: str) -> None:
@@ -24,11 +24,22 @@ class Downloader:
 
 
     def __set_video_streams(self, streams) -> None:
-        array_streams = []
-        for stream in streams:
-            array_streams.append(stream)
-        print(array_streams)
+        array_streams = self.__build_video_streams_object(streams)
         self.streams = array_streams
+
+
+    def __build_video_streams_object(self, streams) -> list:
+        arr_video_streams = []
+        for stream in streams:
+            video_stream_obj = {
+                "itag": stream.itag,
+                "mime_type": stream.mime_type,
+                "resolution": stream.resolution,
+                "fps": stream.fps,
+                "video_type": stream.type,
+            }
+            arr_video_streams.append(video_stream_obj)
+        return arr_video_streams
 
 
     def download(self, itag):
@@ -42,6 +53,6 @@ class Downloader:
     def build_video_data(self) -> dict:
         return {
             "title": self.video_title,
-            "thumbnail_url": self.thumbnail_url
-            # "streams": self.streams
+            "thumbnail_url": self.thumbnail_url,
+            "streams": self.streams
         }
